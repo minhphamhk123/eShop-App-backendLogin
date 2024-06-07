eb763d75bdf69de64b5043ae967c97c1eb6e9209
qv2002 <20520866@gm.uit.edu.vn>
2024-06-07 15:01:49 +0700

    fix authGoogle

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from 'express';
+import express from "express";
 
-import { connectDB } from './config/db.js';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-import cors from 'cors';
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import swaggerSpec from './utils/swagger.js'
-import test, { verifyPolling } from './controllers/auth.controller.js';
+import swaggerSpec from "./utils/swagger.js";
+import test, { verifyPolling } from "./controllers/auth.controller.js";
 
-import { Server } from 'socket.io';
-import eventEmitter from './utils/eventEmitter.js';
+import { Server } from "socket.io";
+import eventEmitter from "./utils/eventEmitter.js";
 
 const __dirname = path.resolve();
 
@@ -32,48 +32,48 @@ app.use(cookieParser());
 const server = app.listen(process.env.PORT || 8080, async () => {
   console.log(`Server running on port ${process.env.PORT || 8080}`);
   try {
-    console.log('⏳ Database connecting...');
+    console.log("⏳ Database connecting...");
     await connectDB;
-    console.log('✅ Database connected.');
+    console.log("✅ Database connected.");
     test;
   } catch (error) {
-    console.log('❌ Error:', error);
+    console.log("❌ Error:", error);
   }
 });
 
 const io = new Server(server, {
   cors: {
     //origin: 'https://doc-depot-by-atanu.vercel.app',
-    origin: 'http://localhost:8080',
-    methods: ['GET', 'POST', 'PATCH'],
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST", "PATCH"],
   },
 });
 
 verifyPolling(io);
 
 // Lắng nghe sự kiện và gửi thông báo qua socket
-eventEmitter.on('userVerifiedStatusChanged', ({ _id, verified }) => {
-  io.emit('verifiedStatus', { _id, verified });
+eventEmitter.on("userVerifiedStatusChanged", ({ _id, verified }) => {
+  io.emit("verifiedStatus", { _id, verified });
 });
 
 // Serve Swagger documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-/**  
-* @openapi
-* /api/auth/signup:
-* post:
-* summary: Sign Up new account
-* description: Post a new account to add to database. Not verified yet. Still on work email verified.
-* parameters:
-* — in: path
-* schema:
-* type: string
-* responses:
-* 200:
-* description: Successful response
-*/
-app.use('/api/auth', authRoutes);
+/**
+ * @openapi
+ * /api/auth/signup:
+ * post:
+ * summary: Sign Up new account
+ * description: Post a new account to add to database. Not verified yet. Still on work email verified.
+ * parameters:
+ * — in: path
+ * schema:
+ * type: string
+ * responses:
+ * 200:
+ * description: Successful response
+ */
+app.use("/api/auth", authRoutes);
 
 //app.use(authCheck);
 app.use("/api/user", userRoutes);
@@ -89,4 +89,3 @@ app.use((err, req, res, next) => {
   });
 });
 //JSON.stringify(preContent, null, 2)
-
